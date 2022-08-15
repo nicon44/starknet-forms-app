017f5a332a0e75973dab71d79d42817534a3393d
ndulio <ndulio@cloudbees.com>
2022-08-15 12:49:00 +0200

    Update form

--- src/hooks/useFormContract.ts
+++ src/hooks/useFormContract.ts
@@ -6,6 +6,6 @@ import FormAbi from '../abi/formAbi.json'
 export function useFormContract() {
   return useContract({
     abi: FormAbi as Abi,
-    address: '0x004820e0c0c0ad748798c94a619c99f94c36814af5ec6fe6b16a6cc8b867aa9f',
+    address: '0x076c82542aeb0fdc6d01009b2012e73ce3e0e879172231fff3cd4acc940900f6',
   })
 }
